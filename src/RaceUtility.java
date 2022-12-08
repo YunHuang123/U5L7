300410c9e2846564b736910553f8ed1b4da9b745
BT_4N2_11 <yhuang6817@bths.edu>
2022-12-08 12:31:50 -0500

    Working on the last problem

---
+++ src/RaceUtility.java
@@ -0,0 +1,27 @@
+public class RaceUtility {
+
+    public static double milesToKm(double miles)
+    {
+        miles = miles * 1.609344;
+        return miles;
+    }
+
+    public static double kmToMiles(double km)
+    {
+        km = km / 1.609344;
+        return km;
+    }
+
+    public static String makeProper(String str)
+    {
+        String words[] = str.split("\\s");
+        String capitalizeWord = "";
+        for(String w:words){
+            String first = w.substring(0,1);
+            String s = w.substring(1);
+            s = s.toLowerCase();
+            capitalizeWord += first.toUpperCase() + s + " ";
+        }
+        return capitalizeWord;
+    }
+}
